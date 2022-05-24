516c835c29a491e92790fdadab2937f5ecee2913
Namrata12gupta <namratag.sass@gmail.com>
2022-05-24 12:42:31 +0530

    second commit error free

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   devise_for :users
-  root 'users#index'
+  root to:'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
   resources :users do
-    resources :attendances
+  resources :attendances
   end
 
   resources :roles

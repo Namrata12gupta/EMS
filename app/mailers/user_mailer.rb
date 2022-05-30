b8c065d360929a85b5aa36228f9d93cb41255fed
Namrata12gupta <namratag.sass@gmail.com>
2022-05-30 14:02:30 +0530

    mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,24 @@
 # frozen_string_literal: true
 
 class UserMailer < ApplicationMailer
-  def create_notification
+  def create_notification(object)
     @object = object
     @object_count = object.class.count
 
-    mail to: 'payal@gmail.com', subject: "A new entry for #{object.class} has one created"
+    mail to: 'user@gmail.com', subject: "A new entry for #{object.class} has one created"
   end
 
-  def update_notification; end
+  def update_notification(object)
+    @object = object
+    @object_count = object.class.count
 
-  def delete_notification; end
+    mail to: 'user@gmail.com', subject: "An entry for #{object.class} has been updated"
+  end
+
+  def delete_notification(object)
+  @object = object
+    @object_count = object.class.count
+
+    mail to: 'user@gmail.com', subject: "An entry for #{object.class} has been deleted" 
+  end
 end
